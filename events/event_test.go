f3159d4a3132eae5f29aaa44343837268132d819
Konrad Kleine <konrad.wilhelm.kleine@gmail.com>
2015-10-28 09:22:43 +0100

    This commit should be held back from being merged into master branch because master branch is protected

--- events/event_test.go
+++ events/event_test.go
@@ -20,30 +20,6 @@ import (
 // into an notifications.Event and report error on success.
 func TestManifestPushEventProcessing(t *testing.T) {
 	eventStr := strings.TrimSpace(`{
-		"id": "asdf-asdf-asdf-asdf-0",
-		"timestamp": "2006-01-02T15:04:05Z",
-		"action": "push",
-		"target": {
-			"mediaType": "application/vnd.docker.distribution.manifest.v1+json",
-			"size": 1,
-			"digest": "sha256:0123456789abcdef0",
-			"length": 1,
-			"repository": "library/test",
-			"url": "http://example.com/v2/library/test/manifests/latest"
-		},
-		"request": {
-			"id": "asdfasdf",
-			"addr": "client.local",
-			"host": "registrycluster.local",
-			"method": "PUT",
-			"useragent": "test/0.1"
-		},
-		"actor": {
-			"name": "test-actor"
-		},
-		"source": {
-			"addr": "hostname.local:port"
-		}
 	}`)
 
 	// Decode JSON string into a notifications.Event structure
